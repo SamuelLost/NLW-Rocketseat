c4db49cd685d04a8572ea7623b69abaaf1bab19e
SamuelLost <samuelhenriq12@gmail.com>
2020-06-03 23:53:18 -0300

    Funções para selecionar vários ítens de coleta

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -19,11 +19,11 @@ function populateUFs() {
     } )
 }
 
-populateUFs()
+populateUFs() //Executando a função
 
 function getCities(event) {
-    const citySelect = document.querySelector("select[name=city]")
-    const stateInput = document.querySelector("input[name=state]")
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
 
     //De forma dinâmica, sempre que mudar a ufValue, muda a url
     const ufValue = event.target.value
@@ -34,13 +34,18 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value=>Selecione a Cidade</option>" //Limpa o conteúdo das cidades
+    citySelect.disabled = true //Bloqueia o campo antes
+
     fetch(url)
     .then( (res) => { return res.json() } ) //Função anonima que retorna valor em json
     .then( (cities) => {
+        
         //Vai pegar um estado do obj json que tem no site e jogar para a constante state
         for(const city of cities) {  
             //Concatenando, pegando o que tinha antes e juntando com o novo
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>` //$ é para interpolar
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>` //$ é para interpolar
+            //Depois de enviar o cadastro, ${city.nome} e ${city.nome} colocam a cidade e o estado na url 
         }
         
         citySelect.disabled = false
@@ -49,4 +54,50 @@ function getCities(event) {
 }
 document
     .querySelector("select[name=uf]")
+    //Ouvidor de eventos
     .addEventListener("change", getCities) //Passando por referência o getCities
+
+//--------------------------------------------------------------------------------------------------------------//
+
+//Ítens de coleta
+//Pegar todos os li's
+
+const itemsToCollect = document.querySelectorAll(".items-grid li") //Pega tudo que estiver dentro
+
+for(const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items")
+
+let selectedItems = [] //Array de items selecionados
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    //Adicionar ou remover uma classe com JavaScript
+    itemLi.classList.toggle("selected") //Possibilitar marcar vários
+    const itemId = itemLi.dataset.id
+
+    //Verificar se existem itens selecionados, se sim
+    //pegar os selecionados
+    const alreadySelected = selectedItems.findIndex( (item) => {
+        const itemFound = item == itemId //Comparando, funciona como if/else
+        return itemFound
+    }) //A constante pega o index do selecionado
+
+    //se já estiver selecionado, tirar da seleção, se não, adicionar 
+    if(alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter( (item) => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+    })
+
+        selectedItems = filteredItems
+    }else {
+        selectedItems.push(itemId) //Coloca um elemento dentro do array
+    }
+
+    //console.log(selectedItems)
+    //atualizar o campo escondido com os itens selecionados 
+    collectedItems.value = selectedItems
+}
